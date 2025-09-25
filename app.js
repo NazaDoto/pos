d5fda984d4620f25e0aa93c0717d67c6396a6960
Naza <nazadoto@gmail.com>
2025-09-25 12:23:36 -0300

    con https

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import multer from "multer";
 import cors from "cors";
 import dotenv from "dotenv";
+import fs from "fs";
+import https from "https";
 
 import resumenRoutes from "./routes/resumen.js";
 import historialRoutes from "./routes/historial.js";
@@ -10,19 +12,35 @@ import authRoutes from "./routes/auth.js";
 import analisisRoutes from "./routes/analisis.js";
 
 dotenv.config();
+
 const app = express();
-app.use(cors());
-app.use(express.json());
 const upload = multer({ dest: "uploads/" });
 
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
+const ENV = process.env.ENV || "dev"; // "dev" o "prod"
 
 // === Rutas ===
 app.use("/auth", authRoutes);
-app.use('/', analisisRoutes);
+app.use("/", analisisRoutes);
 app.use("/", resumenRoutes);
 app.use("/", historialRoutes);
 app.use("/", pdfRoutes);
 
-// Servidor
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+if (ENV === "prod") {
+  // Rutas a los certificados de Let's Encrypt o ACME
+  const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/pos.nazadoto.com/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/pos.nazadoto.com/fullchain.pem"),
+  };
+
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`🚀 Servidor HTTPS corriendo en https://localhost:${PORT}`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor HTTP corriendo en http://localhost:${PORT}`);
+  });
+}
