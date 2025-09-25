b7accff3d9a3e2471e0fe48571df6dcd1eaa168d
Naza <nazadoto@gmail.com>
2025-09-25 12:34:39 -0300

    db

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,9 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
-
 export const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "2112",
+  password: process.env.ENV == "prod" ? process.env.MYSQL_PASSWORD : "2112",
   database: process.env.MYSQL_DATABASE || "pos",
 });
