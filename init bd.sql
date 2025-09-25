14a2e5289148a0a182872276aa202cc5a0ff5b12
Naza <nazadoto@gmail.com>
2025-09-25 11:31:25 -0300

    script bd

---
+++ init bd.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE `pos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+use pos;
+CREATE TABLE `historial` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `analisis` longtext NOT NULL,
+  `resumen` longtext NOT NULL,
+  `fecha` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `precio` varchar(50) DEFAULT NULL,
+  `stock` varchar(50) DEFAULT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `habilitado` tinyint(1) DEFAULT '0',
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
